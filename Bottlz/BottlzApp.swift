ae3100ca4ad634dfbde81ec49343255f56d69d16
Changyuan Lin <coreballium@outlook.com>
2022-10-26 18:48:35 -0400

    Initial Commit

---
+++ Bottlz/BottlzApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottlzApp.swift
+//  Bottlz
+//
+//  Created by Changyuan Lin on 10/26/22.
+//
+
+import SwiftUI
+
+@main
+struct BottlzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
