92081486b886b162744c9376796a4fe69229a17e
Changyuan Lin <coreballium@outlook.com>
2022-11-22 22:21:55 -0500

    Decode route data for bottles

---
+++ Bottlz/Route.swift
@@ -0,0 +1,13 @@
+//
+//  Route.swift
+//  Bottlz
+//
+//  Created by Changyuan Lin on 11/21/22.
+//
+
+import Foundation
+
+struct Route: Codable {
+    var distance: Double
+    var route: [[Double]]
+}
