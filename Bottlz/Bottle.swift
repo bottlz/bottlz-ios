ffd1918c5910c5a40526cfaf454a7234777dd968
Changyuan Lin <coreballium@outlook.com>
2022-11-23 22:13:18 -0500

    Animate bottles following route on map

--- Bottlz/Bottle.swift
+++ Bottlz/Bottle.swift
@@ -9,6 +9,8 @@ import Foundation
 import MapKit
 
 struct Bottle: Codable, Identifiable {
+    static let speed = 1.0
+
     var id: UUID
     var created: Date
 
@@ -45,6 +47,27 @@ struct Bottle: Codable, Identifiable {
                                 point: point, portion: Double(pointIndex) / Double(route.route.count))
             }
         }
+    }
+
+    func computeCurrentLocation(currentDate: Date) -> CLLocationCoordinate2D {
+        let coveredDistance = currentDate.timeIntervalSince(self.created) * Bottle.speed
+
+        var accumulatedDistance = 0.0
+        var currentRouteIndex = routes.count - 1
+        for i in 0..<routes.count {
+            if accumulatedDistance + routes[i].distance > coveredDistance {
+                currentRouteIndex = i
+                break
+            }
+            accumulatedDistance += routes[i].distance
+        }
+
+        let currentRouteTraveledDistance = coveredDistance - accumulatedDistance
+        let currentRouteFraction = currentRouteTraveledDistance / routes[currentRouteIndex].distance
+        print("currentRouteIndex:\(self.id):\(currentRouteIndex)")
+        print("currentRouteFraction:\(self.id):\(currentRouteFraction)")
+
+        return routes[currentRouteIndex].position(fraction: currentRouteFraction)
     }
 
     private enum CodingKeys: String, CodingKey {
